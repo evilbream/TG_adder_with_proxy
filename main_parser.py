0ca18289e077b6763ddf90097cfde4872ad8bb60
Bream <48747024+evilbream@users.noreply.github.com>
2023-08-03 07:36:50 -0700

    Update main_parser.py

--- main_parser.py
+++ main_parser.py
@@ -3,21 +3,20 @@ import os
 from Parser import Parser
 import asyncio
 from assist_func import add_to_existing_file, convert_to_csv
-
+from account_manager import auth_for_parsing
 
 async def parser():
-    if not os.path.exists('tg_accs.csv'):
-        convert_to_csv('tg_accs.txt', 'tg_accs.csv', 'accounts')
-    if not os.path.exists ('proxy.csv'):
-        convert_to_csv('proxy.txt', 'proxy.csv', 'accounts')
+    client = await auth_for_parsing()
+    if client is None:
+        return
     if not os.path.exists('users.csv'):
         open('users.csv', 'w')
     limit = input('How many users do u want to parse? Press enter if all (tg limit is 10000 from one group): ')
     if limit.isdigit():
         limit = int(limit)
-        users = await Parser().parse_members (limit=limit)
+        users = await Parser(client).parse_members (limit=limit)
     else:
-        users = await Parser().parse_members()
+        users = await Parser(client).parse_members()
 
     add_to_existing_file(users)
     print('successfully parsed')
