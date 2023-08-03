c1a08dfd8a72cbeb9e726d3a543de156e14ac704
Bream <48747024+evilbream@users.noreply.github.com>
2023-08-03 07:33:30 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,16 +1,18 @@
-from telethon import TelegramClient
-import asyncio
+from telethon import TelegramClient, connection
 
 
 class Authorisation:
-    def __init__(self, name, api_id, api_hash, phone, proxy=None):
+    def __init__(self, name, api_id, api_hash, phone, proxy=None, mtproxy=None):
         self.proxy = proxy
+        self.mtproxy = mtproxy
         self.phone = phone
         self.name = name
         self.api_hash = api_hash
         self.api_id = api_id
         if self.proxy is not None:
             self.client = TelegramClient(self.name, self.api_id, self.api_hash, proxy=self.proxy)
+        elif self.mtproxy is not None:
+            self.client = TelegramClient(self.name, self.api_id, self.api_hash, connection=connection.ConnectionTcpMTProxyRandomizedIntermediate, proxy=mtproxy)
         else:
             self.client = TelegramClient (self.name, self.api_id, self.api_hash)
 
@@ -20,8 +22,10 @@ class Authorisation:
             await self.client.start (self.phone)
             if self.proxy is not None:
                 print(f'Successful log in from {self.name} per {self.proxy["addr"]} proxy')
+            elif self.mtproxy is not None:
+                print(f'Successful log in from {self.name} per {self.mtproxy[0]} proxy')
             else:
-                print (f'Successful log in from {self.name}')
+                print (f'Successful log in from {self.name} without proxy')
             return self.client
         except Exception as err:
             print(f'Something went wrong, failed to connect to {self.name} cuz {err}')
