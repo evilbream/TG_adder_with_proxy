670025addd624743af22ea0116d742fb491b54a2
Bream <48747024+evilbream@users.noreply.github.com>
2023-09-05 08:52:13 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,8 @@
+import sqlite3
+import os
 from telethon import TelegramClient, connection
 import TelethonFakeTLS
+import telethon.errors
 # now with 2fa support
 class Authorisation:
     def __init__(self, name, api_id, api_hash, phone, proxy=None, mtproxy=None, new_mtproxy=None,
@@ -21,21 +24,57 @@ class Authorisation:
         else:
             self.client = TelegramClient (self.name, self.api_id, self.api_hash, device_model=device_model, system_version=system_version, app_version=app_version)
 
-    async def starts(self) -> TelegramClient or None:
-        print(f'Starting log in to {self.name}')
+    async def starts(self, filter_banned=False) -> TelegramClient or None:
+        if filter_banned:
+            path_dir = await self.filter_banned_accounts()
+            if path_dir:
+                return path_dir
+        else:
+            print(f'Starting log in to {self.name}')
+            try:
+                await self.client.start (self.phone, password=self.password)
+                if self.proxy is not None:
+                    print(f'Successful log in from {self.name} per {self.proxy["addr"]} proxy')
+                elif self.mtproxy is not None:
+                    print(f'Successful log in from {self.name} per {self.mtproxy[0]} proxy')
+                else:
+                    print (f'Successful log in from {self.name} without proxy')
+                return self.client
+            except Exception as err:
+                print(f'Something went wrong, failed to connect to {self.name} cuz {err}')
+                return None
+
+    async def filter_banned_accounts(self):
         try:
             await self.client.start (self.phone, password=self.password)
-            if self.proxy is not None:
-                print(f'Successful log in from {self.name} per {self.proxy["addr"]} proxy')
-            elif self.mtproxy is not None:
-                print(f'Successful log in from {self.name} per {self.mtproxy[0]} proxy')
-            else:
-                print (f'Successful log in from {self.name} without proxy')
-            return self.client
+            return False
+        except telethon.errors.PhoneNumberBannedError:
+            dir_path = sql_del_account(self.api_id)
+            return dir_path
         except Exception as err:
             print(f'Something went wrong, failed to connect to {self.name} cuz {err}')
-            return None
+            return False
 
+
+def sql_del_account(api_id):
+    conn = sqlite3.connect ('accounts.db')
+    cur = conn.cursor ()
+    cur.execute (
+        '''CREATE TABLE IF NOT EXISTS tg_ac (Name TEXT, API_ID INTEGER PRIMARY KEY NOT NULL, API_HASH TEXT, 
+        Phone TEXT, PROXY TEXT, System TEXT, Password TEXT)''')
+    cur.execute('''SELECT Name FROM tg_ac
+    WHERE API_ID == ?''', (api_id, ))
+    res = cur.fetchone()
+    print(res)
+    if res is not None:
+        cur.execute ('''DELETE FROM tg_ac 
+                    WHERE API_ID == ?''', (api_id,))
+        print(f'Account with the name {res[0]} was successfully deleted')
+        conn.commit()
+        return res[0]
+    else:
+        print('There is no account with such an api id')
+    conn.close()
 
 if __name__ == '__main__':
     pass
